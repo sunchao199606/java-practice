32576d01e042b42ddb4a25e842d040d7474c0fe8
sunchao199606 <17854230810@163.com>
2019-08-20 11:13:22 +0800

    变长参数

---
+++ src/main/java/cn/com/sunchao/other/Argument.java
@@ -0,0 +1,27 @@
+package cn.com.sunchao.other;
+
+/**
+ * @author SunChao
+ *
+ * @date 2019年8月15日
+ *
+ * @description 
+ */
+public class Argument {
+	
+	private static void showStrings (String... strings) {
+		for (String string :strings) {
+			System.out.println(string);
+		}
+	}
+	
+	public static void main(String[] args) {
+		String[] strings = new String[] {"1","2"};
+		String string = new String();
+		// List<String> stringList = new ArrayList<String>();
+		// showStrings(stringList);
+		showStrings(strings);
+		showStrings(string);
+	}
+
+}

e6823e3b4d0f7ee1711beb1107ae2f98b19f2fb8
sunchao199606 <17854230810@163.com>
2019-10-09 09:42:31 +0800

    转换为idea工程

--- src/main/java/cn/com/sunchao/java8/DateTimeDemo.java
+++ src/main/java/cn/com/sunchao/java8/DateTimeDemo.java
@@ -1,11 +1,51 @@
 package cn.com.sunchao.java8;
 
-import java.time.LocalTime;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.TemporalAmount;
+import java.time.temporal.TemporalQuery;
+import java.time.temporal.TemporalUnit;
+import java.util.List;
+
+import cn.com.sunchao.encode.CompareEncode;
 
 public class DateTimeDemo {
-	
-	public boolean timeCompare() {
-		LocalTime time1 = LocalTime.of(22, 0);
-		return time1.plusHours(10).compareTo(LocalTime.of(9, 0)) == -1;
+
+	private static void localDateTime() {
+		LocalDate date = LocalDate.now();
+		// 计算本日期所属周的周几日期
+		System.out.println(date.with(ChronoField.DAY_OF_WEEK, 5));
+		// 计算本日期添加10天之后的日期
+		System.out.println(date.with(t -> t.plus(10, ChronoUnit.DAYS)));
+		System.out.println(date.with(t -> t.plus(10, ChronoUnit.MONTHS)));
+		System.out.println(date.with(TemporalAdjusters.lastDayOfYear()));
+		System.out.println(date.range(ChronoField.DAY_OF_YEAR));
+		System.out.println(date.until(date.with(TemporalAdjusters.lastDayOfYear()), ChronoUnit.DAYS));
+	}
+
+	private static void period() {
+		Period period = Period.between(LocalDate.of(2019, 9, 19), LocalDate.of(2019, 10, 18));
+
+		System.out.println(period.get(ChronoUnit.MONTHS));
+		System.out.println(period.getChronology());
+		System.out.println(period.getUnits());
+		Duration duration = Duration.of(2l, ChronoUnit.MONTHS);
+		System.out.println(duration.toDays());
+	}
+
+	private static void compare() {
+		System.out.println(LocalDate.of(2019, 9, 18).compareTo(LocalDate.of(2019, 9, 19)));
+	}
+
+	public static void main(String[] args) {
+
+		// localDateTime();
+		// period();
+		compare();
 	}
 }

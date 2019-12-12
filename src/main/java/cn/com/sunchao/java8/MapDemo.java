e7f4733cd3e7ae691521200f4d7c5f2fbbab7518
sunchao199606 <17854230810@163.com>
2019-12-12 14:48:14 +0800

    Map

--- src/main/java/cn/com/sunchao/java8/MapDemo.java
+++ src/main/java/cn/com/sunchao/java8/MapDemo.java
@@ -4,12 +4,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MapDemo {
-	
-	public static void main(String[] args) {
-		Map<String,Integer> map = new HashMap<>();
-		map.put("sun", 23);
-		map.put("wang", 19);
-		map.put("gao", 34);
-	}
+
+    public static void main(String[] args) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("sun", 23);
+        map.put("wang", 19);
+        map.put("gao", 34);
+        map.compute("sun", (k, v) -> v = v + 1);
+        map.computeIfAbsent("11", k -> 10);
+        map.computeIfPresent("11", (k, v) -> v = 100);
+        System.out.println(map.get("11"));
+    }
 
 }

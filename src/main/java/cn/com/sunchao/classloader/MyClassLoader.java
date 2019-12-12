b14dd301e7810d344e020156e01ca0ec939664e7
sunchao199606 <17854230810@163.com>
2019-12-12 14:47:15 +0800

    ClassLoader测试

---
+++ src/main/java/cn/com/sunchao/classloader/MyClassLoader.java
@@ -0,0 +1,20 @@
+package cn.com.sunchao.classloader;
+
+/**
+ * @author ：sunchao
+ * @date ：Created in 2019/12/6 13:45
+ * @description：ClassLoader测试
+ */
+public class MyClassLoader extends ClassLoader {
+
+    @Override
+    public Class<?> loadClass(String name) throws ClassNotFoundException {
+        return super.loadClass(name);
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        MyClassLoader loader = new MyClassLoader();
+        Class clazz = loader.loadClass("cn.com.sunchao.classloader.SomeClass");
+        clazz.newInstance();
+    }
+}

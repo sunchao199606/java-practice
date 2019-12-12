3db054eb1662ef0a3d1560e301b9b116095037f7
sunchao199606 <17854230810@163.com>
2019-12-12 14:46:56 +0800

    数字类型强制转换

---
+++ src/main/java/cn/com/sunchao/other/IntDemo.java
@@ -0,0 +1,12 @@
+package cn.com.sunchao.other;
+
+/**
+ * @author ：sunchao
+ * @date ：Created in 2019/11/20 16:28
+ * @description：数字类型强制转换
+ */
+public class IntDemo {
+    public static void main(String[] args) {
+        System.out.println((10.0 / 5));
+    }
+}

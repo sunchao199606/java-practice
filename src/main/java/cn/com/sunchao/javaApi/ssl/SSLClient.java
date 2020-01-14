eb66d679fa4eab86032f13bfd5a92040a5a6d619
sunchao199606 <17854230810@163.com>
2020-01-14 11:26:32 +0800

    ssl加密测试

---
+++ src/main/java/cn/com/sunchao/javaApi/ssl/SSLClient.java
@@ -0,0 +1,9 @@
+package cn.com.sunchao.javaApi.ssl;
+
+/**
+ * @author ：sunchao
+ * @date ：Created in 2020/1/7 15:29
+ * @description：SSLClient
+ */
+public class SSLClient {
+}
---
+++ src/main/java/cn/com/sunchao/javaApi/ssl/SSLServer.java
@@ -0,0 +1,9 @@
+package cn.com.sunchao.javaApi.ssl;
+
+/**
+ * @author ：sunchao
+ * @date ：Created in 2020/1/7 15:35
+ * @description：SSLServer
+ */
+public class SSLServer {
+}

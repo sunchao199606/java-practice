59fa623d2aa7d46a4e808a0eb43f9bdb2638483d
sunchao199606 <17854230810@163.com>
2020-01-16 09:34:13 +0800

    JDK动态代理解析

---
+++ src/main/java/cn/com/sunchao/reflection/dynamicProxy/jdk/ProductServiceImpl.java
@@ -0,0 +1,14 @@
+package cn.com.sunchao.reflection.dynamicProxy.jdk;
+
+/**
+ * @author ：sunchao
+ * @date ：Created in 2020/1/15 16:33
+ * @description：被代理类
+ */
+public class ProductServiceImpl implements ProductService {
+
+    @Override
+    public void addProduct(String productName) {
+        System.out.println("正在添加" + productName);
+    }
+}

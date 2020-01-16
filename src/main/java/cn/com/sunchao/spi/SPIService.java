c1cb17eb25c869f6bae931ffd9a323b489a5af37
sunchao199606 <17854230810@163.com>
2020-01-16 17:23:36 +0800

    SPI示例

---
+++ src/main/java/cn/com/sunchao/spi/SPIService.java
@@ -0,0 +1,10 @@
+package cn.com.sunchao.spi;
+
+/**
+ * @author ：sunchao
+ * @date ：Created in 2020/1/16 16:38
+ * @description：interface
+ */
+public interface SPIService {
+    void execute();
+}
---
+++ src/main/java/cn/com/sunchao/spi/Test.java
@@ -0,0 +1,29 @@
+package cn.com.sunchao.spi;
+
+import sun.misc.Service;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
+
+/**
+ * @author ：sunchao
+ * @date ：Created in 2020/1/16 16:44
+ * @description：SPI Test
+ */
+public class Test {
+    public static void main(String[] args) {
+        Iterator<SPIService> providers = Service.providers(SPIService.class);
+        while (providers.hasNext()) {
+            SPIService ser = providers.next();
+            ser.execute();
+        }
+
+        System.out.println("--------------------------------");
+        ServiceLoader<SPIService> load = ServiceLoader.load(SPIService.class);
+        Iterator<SPIService> iterator = load.iterator();
+        while (iterator.hasNext()) {
+            SPIService ser = iterator.next();
+            ser.execute();
+        }
+    }
+}

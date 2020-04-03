041549be70d9307af9ccda4f1f048396c02f510f
sunchao199606 <17854230810@163.com>
2020-04-04 01:40:20 +0800

    Fibonacci

---
+++ src/main/java/cn/com/sunchao/algorithm/Fibonacci.java
@@ -0,0 +1,19 @@
+package cn.com.sunchao.algorithm;
+
+/**
+ * @Description : 斐波那契数列
+ * @Author :sunchao
+ * @Date: 2020-04-01 15:22
+ */
+public class Fibonacci {
+
+    public int fibonacci(int n) {
+        if (n == 1) {
+            return 0;
+        }
+        if (n == 2) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}

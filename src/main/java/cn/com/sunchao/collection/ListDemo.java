b6142a293ddfcf1ecb14a7afd1f7eacf3e1da176
sunchao199606 <17854230810@163.com>
2020-04-16 00:23:36 +0800

    Arrays.copyOf方法测试：直接返回一个新的数组，并将原数组的元素复制到其中

---
+++ src/main/java/cn/com/sunchao/collection/ListDemo.java
@@ -0,0 +1,17 @@
+package cn.com.sunchao.collection;
+
+import java.util.Arrays;
+
+/**
+ * @Description : ListDemo
+ * @Author :sunchao
+ * @Date: 2020-04-16 00:02
+ */
+public class ListDemo {
+
+    public static void main(String[] args) {
+        //List<Integer> list = new ArrayList<>();
+        Integer[] integers = new Integer[10];
+        System.out.println(integers == Arrays.copyOf(integers, 20));
+    }
+}

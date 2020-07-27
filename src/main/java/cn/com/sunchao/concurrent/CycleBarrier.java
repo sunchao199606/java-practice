3fef9461c53aed8decc96317713d5f8bad7ae2ca
sunchao199606 <17854230810@163.com>
2020-07-27 23:53:10 +0800

    wait notify实现线程执行barrier

--- src/main/java/cn/com/sunchao/concurrent/CycleBarrier.java
+++ src/main/java/cn/com/sunchao/concurrent/CycleBarrier.java
@@ -4,28 +4,60 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * 当等待的线程个数达到指定数据的时候notifyAll
+ */
 public class CycleBarrier {
 
-	@SuppressWarnings("static-access")
-	public static void main(String[] args) {
-		CyclicBarrier barrier = new CyclicBarrier(10);
-		AtomicInteger id = new AtomicInteger(0);
-		for (int i = 0; i < 8; i++) {
-			Thread thread = new Thread(() -> {
-				try {
-					barrier.await();
-				} catch (InterruptedException | BrokenBarrierException e) {
-					e.printStackTrace();
-				}
-				System.out.println("player " + id.getAndIncrement() + " run");
-			});
-			thread.start();
-		}
-		try {
-			Thread.currentThread().sleep(3000l);
-			barrier.await();
-		} catch (InterruptedException | BrokenBarrierException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        CyclicBarrier barrier = new CyclicBarrier(10);
+        AtomicInteger id = new AtomicInteger(0);
+        for (int i = 0; i < 9; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    barrier.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("player " + id.getAndIncrement() + " run");
+            });
+            thread.start();
+        }
+        try {
+            Thread.currentThread().sleep(3000l);
+            barrier.await();
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+        barrier();
+    }
+
+
+    private static void barrier() {
+        Object obj = new Object();
+        for (int i = 0; i < 10; i++) {
+            final int ref = i;
+            Thread thread = new Thread(() -> {
+                synchronized (obj) {
+                    try {
+                        System.out.println("Thread " + ref + " waiting");
+                        obj.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("Thread " + ref + " run");
+            });
+            thread.start();
+        }
+        try {
+            Thread.sleep(3000l);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("main thread notifyAll after 3s");
+        synchronized (obj) {
+            obj.notifyAll();
+        }
+    }
 }

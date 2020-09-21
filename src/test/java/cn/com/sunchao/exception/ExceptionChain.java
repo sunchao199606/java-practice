132ff5fe26f8c6f4fd68bca2f55abaec4dabfd6f
sunchao199606 <17854230810@163.com>
2020-09-21 15:43:22 +0800

    异常链测试

---
+++ src/test/java/cn/com/sunchao/exception/ExceptionChain.java
@@ -0,0 +1,22 @@
+package cn.com.sunchao.exception;
+
+/**
+ * @Description : 异常链
+ * @Author : mockingbird
+ * @Date : 2020/9/21 14:38
+ */
+public class ExceptionChain {
+
+    public static void fun() throws Exception {
+        try {
+            System.out.println(1 / 0);
+        } catch (Exception e) {
+            throw new Exception("aaaa", e);
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        fun();
+    }
+}

b7df64c1c929f5553d54a8676bf0d4a3c072c422
sunchao199606 <17854230810@163.com>
2020-12-15 22:29:54 +0800

    jcef测试

---
+++ jcef/src/main/java/cn/com/sunchao/cef/CefClientImpl.java
@@ -0,0 +1,12 @@
+package cn.com.sunchao.cef;
+
+/**
+ * @Description :
+ * @Author : mockingbird
+ * @Date : 2020/12/9 15:17
+ */
+public class CefClientImpl {
+    public static void main(String[] args) {
+
+    }
+}

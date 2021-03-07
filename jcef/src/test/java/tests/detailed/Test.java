94a79dda0338d69f2548f98c28e242cf92a0aa05
sunchao199606 <17854230810@163.com>
2021-03-07 13:14:30 +0800

    get

---
+++ jcef/src/test/java/tests/detailed/Test.java
@@ -0,0 +1,13 @@
+package tests.detailed;
+
+/**
+ * @Description :
+ * @Author : mockingbird
+ * @Date : 2021/2/24 16:47
+ */
+public class Test {
+    private Object object = null;
+    public void get() {
+        //this.object = object || new Object();
+    }
+}

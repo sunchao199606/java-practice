b05aa5b74948c77bf1171d0974b325562e975be8
sunchao199606 <17854230810@163.com>
2021-11-09 15:54:25 +0800

    test length

---
+++ src/main/java/cn/com/sun/javaApi/Constructor.java
@@ -0,0 +1,19 @@
+package cn.com.sun.javaApi;
+
+class Parent {
+    Parent(int a) {
+    }
+
+    public Parent() {
+
+    }
+}
+
+class Son extends Parent {
+    Son() {
+    }
+}
+
+public class Constructor {
+
+}

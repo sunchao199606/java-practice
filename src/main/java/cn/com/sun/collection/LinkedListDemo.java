f955570b83e5b41718b80e8c174f4d998ec8283c
sunchao199606 <17854230810@163.com>
2021-11-09 15:54:57 +0800

    LinkedListDemo.java

---
+++ src/main/java/cn/com/sun/collection/LinkedListDemo.java
@@ -0,0 +1,15 @@
+package cn.com.sun.collection;
+
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(1);
+        linkedList.add(1);
+        linkedList.add(1);
+        linkedList.add(1);
+
+    }
+}
